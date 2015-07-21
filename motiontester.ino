d832ea06208978bd146d4241ecb7155102328edd
Rachel <rachel.chloe.gibbs@gmail.com>
2015-07-21 08:26:42 -0500

    Create motiontester.ino

---
+++ motiontester.ino
@@ -0,0 +1,17 @@
+#define PIR_MOTION_SENSOR 3//Use pin 3 to receive the signal from the module 
+
+void setup()
+{
+	Serial.begin(9600);
+	pinMode(PIR_MOTION_SENSOR, INPUT);
+}
+
+void loop() 
+{
+	int sensorValue = digitalRead(PIR_MOTION_SENSOR);
+
+	if(sensorValue==HIGH)//if it detects moving people
+		Serial.println("Motion Detected");
+	else
+		Serial.println("No Movement");
+}
